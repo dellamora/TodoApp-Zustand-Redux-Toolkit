f7496ba33168699b1e91e36f1faa18299f3001e6
Dellamora <francielle@dellamora.dev>
2023-03-17 01:49:47 -0300

    added new pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
 import { type NextPage } from "next";
-import { AddTodoForm } from "~/redux/addTodo";
-import { TodoList } from "~/redux/todoList";
+import Link from "next/link";
 
 
 const Home: NextPage = () => {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center  bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            My <span className="text-[hsl(280,100%,70%)]">TODO</span> App
-          </h1> 
-
-          <AddTodoForm />
-          <TodoList />
+      <main className="flex min-h-screen flex-col items-center justify-center  bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center gap-12 px-4 py-16 ">
+          <Link href="/redux" className="text-white text-5xl font-extrabold tracking-tight  sm:text-[5rem]">
+            Redux
+          </Link>
+          <Link href="/zustand"  className="text-[hsl(280,100%,70%)] text-5xl font-extrabold tracking-tight  sm:text-[5rem]">
+            Zustand
+          </Link>
         </div>
       </main>
     </>
---
+++ src/pages/zustand.tsx
@@ -0,0 +1,23 @@
+import { type NextPage } from "next";
+import { AddTodoForm } from "~/redux/addTodo";
+import { TodoList } from "~/redux/todoList";
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center  bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            My <span className="text-[hsl(280,100%,70%)]">TODO</span> App
+          </h1> 
+
+          <AddTodoForm />
+          <TodoList />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
